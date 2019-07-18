d7b5bc258502261492b7335b6e23a914abaa34d6
SakamotoMakishi <dauw.mf81@gmail.com>
2019-07-19 02:27:09 +0900

    自動更新機能

--- app/assets/javascripts/message-ajax.js
+++ app/assets/javascripts/message-ajax.js
@@ -1,9 +1,8 @@
 $(function(){
-
   function messageHTML(message){
-    var image = message.image ? `<img src= ${message.image} >` : "";
-    var message_text = message.content ? `${message.content}` : "";
-    var html = `<div class="message">
+    var image = message.image ? `<img src= ${message.image} >` : "";  //message.imageにtrueならHTML要素、faiseなら空の値を代入。
+    var message_text = message.content ? `${message.content}` : "";  // 同上 三項演算子
+    var html = `<div class="message" data-message-id="${message.id}">
                   <div class="upper-info">
                     <div class="upper-info__user">
                       ${message.name}
@@ -20,10 +19,10 @@ $(function(){
                     </div>
                   </div>`
     return html;
-    }
-
+  }
+  //非同期通信の記述
   $('#new_message').on('submit',function(e){
-    e.preventDefault();
+    e.preventDefault();     //submitされた処理を止める。
     var message = new FormData(this);
     var url = $(this).attr('action');
     $.ajax({
@@ -35,17 +34,41 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      var html = messageHTML(message);
-      $('.messages').append(html)
-      $('#new_message')[0].reset();
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      var html = messageHTML(message);  //messageHTMLを引数でappendに渡す。↓
+      $('.message_list').append(html)     //引数を受け取る。↑
+      $('#new_message')[0].reset();  //text送信後入力した値を消す。
+      $('.message_list').animate({ scrollTop: $('.message_list')[0].scrollHeight});  //メッセージ入力後スクロールで一番下まで戻す。
       return false
     })
     .fail(function(){
       alert('メッサージを入力してください');
     })
-    .always(function(){
+    .always(function(){ //リロードせずにSENDボタンを推せる
       $('.submit').prop('disabled', false);
     })
-  })
+    //自動更新の記述
+    var reloadMessages = function(){
+      var last_message_id = $('.message:last').data('message-id');//カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+        $.ajax({
+          url: "api/messages",//サーバー指定
+          type: 'GET',  //ルーティングで設定
+          dataType: 'json',  //ルーティングで設定したURLを指定
+          data: {last_id: last_message_id}   //dataオプションでリクエストに値を含める
+        })
+        .done(function(messages){
+          var insertHTML = '';//追加するHTMLの入れ物を作る
+            messages.forEach(function(message){//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+            insertHTML = messageHTML(message);//メッセージが入ったHTMLを取得
+            $('.message_list').append(insertHTML);//メッセージを追加
+            $('.message_list').animate({ scrollTop: $('.message_list')[0].scrollHeight});
+          })
+        })
+        .fail(function() {
+          alert('自動更新失敗しました！');
+        })
+      }
+    setInterval(reloadMessages, 3000);
+  });
 });
+
+
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,6 @@
-json.name @message.user.name
+#非同期通信のjbuilder
+json.name    @message.user.name
 json.content @message.content
 json.date    @message.created_at.strftime("%Y/%m/%d %H:%M")
 json.image   @message.image.url
+json.id      @message.id
